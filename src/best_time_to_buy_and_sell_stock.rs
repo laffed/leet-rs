8f54a81795586f91eb27ba2fa8fdc6ad876aa1ca
laffed <roarklaffed@gmail.com>
2023-07-29 09:45:48 -0500

    .

--- src/best_time_to_buy_and_sell_stock.rs
+++ src/best_time_to_buy_and_sell_stock.rs
@@ -1,4 +1,7 @@
+#![allow(dead_code)]
+
 /*
+#121 Best Time to Buy and Sell Stock
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
@@ -8,7 +11,7 @@ Constraints:
 0 <= prices[i] <= 10^4
 */
 
-fn solution<P>(prices: P) -> i32
+pub fn solution<P>(prices: P) -> i32
 where
     P: IntoIterator<Item = i32> + Copy,
 {
