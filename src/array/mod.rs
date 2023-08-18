514862bcf20138572a7bbfae66b44deca80c6081
laffed <roarklaffed@gmail.com>
2023-08-18 16:00:03 -0500

    15 init

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -2,4 +2,5 @@ mod best_time_to_buy_and_sell_stock;
 mod container_with_most_water;
 mod contains_duplicate;
 mod product_of_array_except_self;
+mod three_sum;
 mod two_sum;
---
+++ src/array/three_sum.rs
@@ -0,0 +1,47 @@
+#![allow(dead_code)]
+
+/*
+#15 3Sum
+
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]] such that
+i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Constraints:
+3 <= nums.length <= 3000
+-10^5 <= nums[i] <= 10^5
+*/
+
+/*
+T:
+S:
+*/
+pub fn solution_a<N>(_nums: N) -> Vec<Vec<i32>> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a_0() {
+        assert_eq!(
+            solution_a(vec![-1, 0, 1, 2, -1, -4]),
+            vec![vec![-1, -1, 2], vec![-1, 0, 1]]
+        );
+    }
+
+    #[test]
+    fn a_1() {
+        let right: Vec<Vec<i32>> = vec![];
+        assert_eq!(solution_a(vec![0, 1, 1]), right);
+    }
+
+    #[test]
+    fn a_2() {
+        assert_eq!(solution_a(vec![0, 0, 0]), vec![vec![0, 0, 0]]);
+    }
+}
